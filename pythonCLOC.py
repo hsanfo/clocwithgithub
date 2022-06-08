d3c52ef083825188c6c36c29aea4e3e19f12d55e
hsanfo <sanfo.halidou@yahoo.fr>
2022-06-08 21:44:19 +0200

    Add gitignore and some cleaning and improvments

--- pythonCLOC.py
+++ pythonCLOC.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 import sys
-import os
 import subprocess
 import smtplib
+import shutil
+import subprocess
+import shutil
+import os
+import stat
+from os import path
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+from datetime import datetime
 
 #****************HELP*****************#
 import argparse
@@ -17,36 +22,52 @@ parser=argparse.ArgumentParser(
 parser.add_argument('repo', default='https://github.com/hsanfo/clocwithgithub.git', help='GITHUB URL IN HTTPS!')
 parser.add_argument('email', default='cloctogithubmail@gmail.com', help='CLOC scan result will be sent to this Email adress!')
 args=parser.parse_args()
-
+print(args)
 #****************HELP*****************#
 
+#*******Clean repo*********#
+def removerepo(rep):
+    for root, dirs, files in os.walk(rep):
+        for dir in dirs:
+            os.chmod(path.join(root, dir), stat.S_IRWXU)
+        for file in files:
+            os.chmod(path.join(root, file), stat.S_IRWXU)
+    shutil.rmtree(rep)
+#*******Clean repo*********#
 tmp = "temp-cloc-repot"
 cloc = "cloc " + tmp
 isExist = os.path.isdir(tmp)
 
 #check if the temp repot file exist
-gitrep = args.repo #sys.argv[1]
-destEmail = args.email # sys.argv[2]
+gitrep = args.repo
+destEmail = args.email
 if isExist:
-    #os.system(cloc)
-    result = subprocess.check_output(cloc,shell=True, stderr=subprocess.STDOUT)
-    print(result)
+    removerepo(tmp)
 
-else:
-    # clone the repot to be scannned by cloc tool.
-    
-    clone = "git clone --depth 1 "+ gitrep+ " " +  tmp 
-    os.system(clone)
-    
-    # lunch cloc
-    #os.system(cloc)
+# clone the repot to be scannned by cloc tool.
+clone = "git clone --depth 1 "+ gitrep+ " " +  tmp
+os.system(clone)
+
+# lunch cloc
+try :
     result = subprocess.check_output(cloc,shell=True, stderr=subprocess.STDOUT)
-    print(result)
-#os.system("python -m smtpd -n -c DebuggingServer localhost:1025")
-#send result to  email
-s=smtplib.SMTP(host='localhost', port=1025)
-s.sendmail("halidsanfo@gmail.com", destEmail, "the cloc scan result : "+str(result, 'utf-8') )
-s.quit()
+    print('\n ***********************result*********************** \n')
+    print(str(result, 'utf-8'))
+    print('\n')
+except:
+    print('May be you should install the cloc tool before :)')
+#remove the temporary cloned repository
+removerepo(tmp)
+
+#send result to  email (for local smtp serveur)
+try :
+    s=smtplib.SMTP(host='localhost', port=1025)
+    s.sendmail("halidsanfo@gmail.com", destEmail, "the cloc scan result : "+str(result, 'utf-8') )
+    s.quit()
+except :
+    tips = 'In LINUX : python -m smtpd -n -c DebuggingServer localhost:1025'
+    print('You may need to start a local smtp server to test the mail sending part of the demo :)')
+    print(tips)
 
 
 ''' 
